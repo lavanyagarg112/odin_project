90a53d63e39f20354082ca9c17d8690f10f8298a
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-11 16:13:19 +0800

    more script added

--- javascript_basics/DOM/Exercise1/script.js
+++ javascript_basics/DOM/Exercise1/script.js
@@ -6,3 +6,22 @@ para.style.color = "red";
 para.textContent = "Hey I am red!";
 
 container.appendChild(para);
+
+const head3 = document.createElement('h3');
+head3.style.color = "blue";
+head3.textContent = "i am a blue h3!"
+container.appendChild(head3);
+
+const parentdiv = document.createElement('div');
+parentdiv.style.cssText = "background: pink; border: black;";
+
+const childhead = document.createElement('h1');
+childhead.textContent = "Im in a div";
+
+const childpara = document.createElement('p');
+childpara.textContent = "ME TOO!";
+
+parentdiv.appendChild(childhead);
+parentdiv.appendChild(childpara);
+
+container.appendChild(parentdiv);
