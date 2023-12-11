a651a6433ea3a6ca75e129e03aa1f82b354a4f91
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-11 16:20:30 +0800

    script edited

--- javascript_basics/DOM/exercise2/script.js
+++ javascript_basics/DOM/exercise2/script.js
@@ -3,3 +3,7 @@ const btn = document.querySelector('#btn');
 btn.addEventListener('click', function (e) {
     console.log(e.target);
   });
+
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+  });
